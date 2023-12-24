fb6567d16846fee10aa3ec9297a7f2005285a6a7
Izzxt <haziqizzat89@gmail.com>
2023-12-24 21:27:05 +0800

    refactor: Refactor client.ts file structure

--- src/client.ts
+++ src/client.ts
@@ -1,34 +1,46 @@
-import { Client, Collection, GatewayIntentBits, GuildEmoji, Partials, ReactionEmoji } from "discord.js";
+import {
+  Client,
+  Collection,
+  GatewayIntentBits,
+  Partials
+} from "discord.js";
 import { Interaction } from "./loaders/interaction";
 import { InteractionHandler } from "./loaders/interactions/handler";
-import { IClient, IEvent } from "./types";
+import { IClient, IEvent, Permission } from "./types";
 
-export const ERROR_MESSAGE = "An error has occured, please report bot administrator";
+export const ERROR_MESSAGE =
+  "An error has occured, please report bot administrator";
 
 export class Bot extends Client implements IClient {
-    public interactionHandler: InteractionHandler;
-    public interaction: Collection<string, Interaction>;
-    public buttons: Collection<string, Interaction>;
-    public menus: Collection<string, Interaction>;
-    public modals: Collection<string, Interaction>;
-    public reactions: Collection<string | null, Interaction>;
-    public event: Collection<string, IEvent>;
-    public interactions: any[] = [];
+  public interactionHandler: InteractionHandler;
+  public interaction: Collection<string, Interaction>;
+  public buttons: Collection<string, Interaction>;
+  public menus: Collection<string, Interaction>;
+  public modals: Collection<string, Interaction>;
+  public reactions: Collection<string | null, Interaction>;
+  public event: Collection<string, IEvent>;
+  public permisions: Permission[] = [];
+  public interactions: any[] = [];
 
-    public constructor() {
-        super({
-            intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.MessageContent],
-            partials: [Partials.Message, Partials.Reaction, Partials.Channel],
-        });
+  public constructor() {
+    super({
+      intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMessageReactions,
+        GatewayIntentBits.MessageContent,
+      ],
+      partials: [Partials.Message, Partials.Reaction, Partials.Channel],
+    });
 
-        this.interaction = new Collection();
-        this.buttons = new Collection();
-        this.menus = new Collection();
-        this.modals = new Collection();
-        this.reactions = new Collection();
-        this.event = new Collection();
+    this.interaction = new Collection();
+    this.buttons = new Collection();
+    this.menus = new Collection();
+    this.modals = new Collection();
+    this.reactions = new Collection();
+    this.event = new Collection();
 
-        this.interactionHandler = new InteractionHandler();
-        this.interactionHandler.load(this);
-    }
+    this.interactionHandler = new InteractionHandler();
+    this.interactionHandler.load(this);
+  }
 }
