4fffce03da0a77c3fd6bdc99fdbe768fbdd5aa3c
Izzxt <haziqizzat89@gmail.com>
2023-12-24 21:25:51 +0800

    refactor: Refactor ready event code

--- src/events/client/ready.ts
+++ src/events/client/ready.ts
@@ -5,23 +5,30 @@ import { logger } from "../../utils/logger";
 import { Events, REST, Routes } from "discord.js";
 
 export default class Ready extends Event {
-    constructor() {
-        super({ name: Events.ClientReady });
+  constructor() {
+    super({ name: Events.ClientReady });
+  }
+
+  public async execute(bot: Bot): Promise<void> {
+    try {
+      const rest = new REST({ version: "8" }).setToken(TOKEN);
+
+      logger.info(
+        `Started refreshing ${bot.interactions.length} application (/) commands.`
+      );
+
+      const data = await rest.put(
+        Routes.applicationGuildCommands(CLIENTID, GUILDID),
+        { body: bot.interactions }
+      );
+
+      if (Array.isArray(data)) {
+        logger.info(
+          `Successfully reloaded ${data.length} application (/) commands.`
+        );
+      }
+    } catch (error) {
+      logger.error(error);
     }
-
-    public async execute(bot: Bot): Promise<void> {
-        try {
-            const rest = new REST({ version: "10" }).setToken(TOKEN);
-
-            logger.info(`Started refreshing ${bot.interactions.length} application (/) commands.`);
-
-            const data = await rest.put(Routes.applicationGuildCommands(CLIENTID, GUILDID), { body: bot.interactions });
-
-            if (Array.isArray(data)) {
-                logger.info(`Successfully reloaded ${data.length} application (/) commands.`);
-            }
-        } catch (error) {
-            logger.error(error);
-        }
-    }
+  }
 }
