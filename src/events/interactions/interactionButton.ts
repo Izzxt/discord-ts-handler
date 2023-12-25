da68b494ede03cec8c3304d2f057d19c4a92b37a
Izzxt <haziqizzat89@gmail.com>
2023-12-25 09:44:28 +0800

    chore: add button interaction functionality

--- src/events/interactions/interactionButton.ts
+++ src/events/interactions/interactionButton.ts
@@ -1,9 +1,21 @@
 import { Events, Interaction } from "discord.js";
 import { Response } from "src/types/types";
+import { logger } from "src/utils/logger";
 import { Bot, ERROR_MESSAGE } from "../../client";
 import { Event } from "../../loaders/event";
-import { logger } from "src/utils/logger";
-import { codeBlock } from "@discordjs/builders";
+import { MY_DECORATOR_METADATA } from "src/interactions/test";
+
+export const classWithDecorator: any[] = [];
+
+export function Inject(constructor: Function) {
+  classWithDecorator.push(constructor);
+}
+
+export function ButtonId(key?: string) {
+  return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    Reflect.defineMetadata(MY_DECORATOR_METADATA, key, target, propertyKey);
+  };
+}
 
 export default class InteractionButton extends Event {
   constructor() {
@@ -15,15 +27,29 @@ export default class InteractionButton extends Event {
       const cmd = bot.buttons.get(interaction.customId);
       if (!cmd) return await interaction.reply({ content: ERROR_MESSAGE, ephemeral: true });
 
-      if (cmd.interactionOpt.buttonIds === undefined) return await interaction.reply({ content: ERROR_MESSAGE, ephemeral: true });
+      if (cmd.interactionOpt.buttonIds === undefined)
+        return await interaction.reply({ content: ERROR_MESSAGE, ephemeral: true });
 
       if (cmd.interactionOpt.buttonIds.includes(interaction.customId)) {
         try {
-          await cmd.executeButtonInteraction?.(bot, interaction, interaction.customId);
+          classWithDecorator.forEach((x) => {
+            const instance = new x();
+            const methods = Object.getOwnPropertyNames(x.prototype).filter((method) => method !== "constructor");
+
+            methods.forEach(async (method) => {
+              if (typeof instance[method] === "function") {
+                const key = Reflect.getMetadata(MY_DECORATOR_METADATA, instance, method);
+                if (key === interaction.customId) {
+                  await instance[method](bot, interaction);
+                }
+              }
+            });
+          });
+          // await cmd.executeButtonInteraction?.(bot, interaction, interaction.customId);
         } catch (error) {
           if (error instanceof Error) {
-            logger.error(error)
-          };
+            logger.error(error);
+          }
         }
       }
     }
--- src/interactions/test.ts
+++ src/interactions/test.ts
@@ -1,15 +1,27 @@
+import { codeBlock } from "@discordjs/builders";
 import {
+  ActionRow,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonInteraction,
+  ButtonStyle,
+  CacheType,
+  ChannelType,
   CommandInteraction,
   InteractionResponse,
   SlashCommandBuilder
 } from "discord.js";
 import { Bot } from "../client";
 import { Interaction } from "../loaders/interaction";
-import { codeBlock } from "@discordjs/builders";
+import { Inject, ButtonId } from "src/events/interactions/interactionButton";
 
+export const MY_DECORATOR_METADATA = "__my_decorator__";
+
+@Inject
 export default class Test extends Interaction {
   constructor() {
     super({
+      buttonIds: ["test-btn"],
       data: new SlashCommandBuilder()
         .setName("test")
         .setDescription("Test something"),
@@ -21,7 +33,21 @@ export default class Test extends Interaction {
     interaction: CommandInteraction,
     ...args: any[]
   ): Promise<InteractionResponse<boolean> | undefined> {
-    let symbol = Symbol("Role")
-    return interaction.reply({ content: codeBlock("json", `${JSON.stringify(symbol, null, 2)}`), ephemeral: true });
+    const btn = new ButtonBuilder()
+      .setCustomId("test-btn")
+      .setLabel("Test Button")
+      .setStyle(ButtonStyle.Secondary);
+
+    const btnRow = new ActionRowBuilder<ButtonBuilder>().addComponents(btn);
+    return interaction.reply({ ephemeral: true, components: [btnRow] });
+  }
+
+  @ButtonId("test-btn")
+  public async testBtn(bot: Bot, interaction: ButtonInteraction<CacheType>) {
+    console.log("test-btn")
+  }
+
+  public async any() {
+    console.log("any")
   }
 }
